e06a12007eb438db6eb6bf85014e213af9836341
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-19 17:19:07 +0500

    Task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+while (number>0)
+{
+    
+    summa = summa + number % 10; // Остаток от деления (%)
+    number = number / 10; // деление числа типа int - остаток не учитывается 
+   
+}
+Console.WriteLine(summa);
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
