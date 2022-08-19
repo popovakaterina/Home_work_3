9e5fc5fcb510467acfebc202d6009776692e7c02
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-19 19:29:18 +0500

    Task 25 edited

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -4,7 +4,16 @@ Console.Write("Введите число А: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число В: ");
 int B = Convert.ToInt32(Console.ReadLine());
-double A_sqr = Math.Pow(A,B);
-Console.WriteLine("Результат: "+ A_sqr);
+int result = A;
+for (int i = 1; i<B; i++) 
+{
+    result = result * A;
+    
+}
+Console.WriteLine(result);
+//double A_sqr = Math.Pow(A,B); - Если делить буз цикла
+//Console.WriteLine("Результат: "+ A_sqr); - Вывод для варианта без цикла
+
+
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
